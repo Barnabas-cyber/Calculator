4c96253d05aed2d89658238161818138ec3994f1
Barnbas-cyber <moloibarnabas@gmail.com>
2022-09-08 15:33:12 +0200

    script testing

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 // calculator functionality
-class Calculator{
-//clear method
-clear(){
-    this.currentOperand = '';
+//computation/results variable
+let computation;
+class Calculator {
+    //clear method
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+
+    }
+
+
+    //addition function
+    addition(previous, current) {
+        computation = previous + current
+        return computation;
+    }
+    //subtration function
+    subtraction() {
+        computation = previous - current
+        return computation
+    }
+    //multiply function
+    multiplication() {
+        computation = previous * current
+        return computation;
+    }
+    // divide function
+    division() {
+        computation = previous / current
+        return computation;
+    }
+    //delete method
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
+
+    //operate function
+    operate() {
+
+    }
 
 }
-
-//delete method
-delete (){
-    this.currentOperand = this.currentOperand.toString().slice(0, -1)
-}
-
-}
-
+console.log(addition(5, 10));
 //varibales representing buttons
 let numberButttons = document.querySelectorAll('[data-number]')
 let operationButtons = document.querySelectorAll('[data-opeartion]')
@@ -21,3 +50,5 @@ let equalButton = document.querySelector('data-equal')
 let clearAllButton = document.querySelector['data-clear-all']
 let currentOperandTextElement = document.querySelector['data-current-operand']
 let previousOperandTextElement = document.querySelector['data-previous-operand']
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
